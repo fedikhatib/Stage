ab345122936ef21a8c65c55c3cb456e15488b3cc
fedikhatib <34138112+fedikhatib@users.noreply.github.com>
2021-09-08 17:25:16 +0100

    first commit

---
+++ stage/MainViewModel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace stage
+{
+    /// <summary>
+    /// Interaction logic for MainViewModel.xaml
+    /// </summary>
+    public partial class MainViewModel : Window
+    {
+
+        public MainViewModel( Plot pl)
+        {
+            InitializeComponent();
+            
+            this.MyModel = new PlotModel { Title = pl.nom };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, Double.Parse(pl.haxe), 10, 0.1, "cos(x)"));
+            DataContext = this;
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+    public class Plot
+    {
+        public string haxe { set; get; }
+        public string vaxe { set; get; }
+        public string nom { set; get; }
+    }
+}
+
+   
---
+++ stage/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace stage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           InitializeComponent();
+           // InitializeComponent();
+
+            //DataContext = new MainViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Plot pl = DataGridXaml.SelectedItem as Plot;
+
+            MainViewModel win = new MainViewModel(pl);
+            win.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Plot pl = new Plot();
+            pl.haxe = haxe.Text;
+            pl.vaxe = vaxe.Text;
+            pl.nom = nom.Text;
+            DataGridXaml.Items.Add(pl);
+
+        }
+        public PlotModel MyModel { get; private set; }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
